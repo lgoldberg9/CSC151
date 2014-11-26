ce6cba2379259961cbd8d880c7925d7514e5a932
Henry Fisher <fisherhe@grinnell.edu>
2014-11-25 22:16:00 -0600

    Documentation woooOgit pull -ull

--- Project.rkt
+++ Project.rkt
@@ -203,6 +203,34 @@
 ; | Effect Procedures |
 ; +-------------------+
 
+;;; Procedure:
+;;;   add-scaled-ellipse!
+;;; Parameters:
+;;;   image, an image
+;;;   base-layer, a positive integer
+;;;   left, an integer
+;;;   top, an integer
+;;;   width, an integer
+;;;   height, an integer
+;;;   aoe, an integer
+;;;   stroke?, a boolean
+;;; Purpose:
+;;;   To add a single ellipse to an image that takes the portion of the image
+;;;   defined by left - aoe, top - aoe, width + 2aoe, height + 2aoe, and 
+;;;   put that portion into an ellipse defined by left, top, width, and height.
+;;;   The scaled ellipse will be stroke if stroke? is true.
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   base-layer is a layer in image
+;;; Postconditions:
+;;;   image now has an ellipse with the following stats:
+;;;    (image-left image) = left
+;;;    (image-top image) = top
+;;;    (image-width image) = width
+;;;    (image-height image) = height
+;;;    (if stroke? (the selection will be stroked)
+;;;                (the selection will not be stroked))
 (define add-scaled-ellipse!
   (lambda (image base-layer left top width height aoe stroke?)
     (copy-and-add-layer! image base-layer)
@@ -213,46 +241,127 @@
                              (+ (* 2 aoe) width)
                              (+ (* 2 aoe) height))
       (when stroke? 
-        (repeat 5 context-set-brush! "2. Hardness 100" 3)
-        (image-stroke-selection! image))
+        (context-set-brush! "2. Hardness 100" 3)
+        (repeat 1 (image-stroke-selection! image)))
       (scale-selection-into-new-layer!
        temp-layer left top (+ left width) (+ top height))
       (gimp-image-remove-layer image temp-layer)
-      (merge-floating-layer image (get-top-layer image) 1))))
+      (merge-floating-layer! image (get-top-layer image) 1))))
 
+;;; Procedure:
+;;;   scale-selection-into-new-layer!
+;;; Parameters:
+;;;   layer, a positive integer
+;;;   top-left-x, a positive integer
+;;;   top-left-y, a positive integer
+;;;   bot-right-x, a positive integer
+;;;   bot-right-y, a positive integer
+;;; Purpose:
+;;;   To scale a selection on layer into a new shape on a new layer.
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   There must be a selection on the image
+;;;   layer is a layer in image
+;;; Postconditions:
+;;;   The previous selection is now scaled into a square (though the
+;;;    shape isn't necessarily a square) defined by the coordinates
+;;;    top-left-x, top-left-y, bot-right-x, bot-right-y.
+;;;   (get-top-layer image) contains this scaled item.
 (define scale-selection-into-new-layer!
-  (lambda (temp-layer top-left-x top-left-y bot-right-x bot-right-y)
+  (lambda (layer top-left-x top-left-y bot-right-x bot-right-y)
     (gimp-floating-sel-to-layer
      (car (gimp-item-transform-scale 
-           temp-layer 
+           layer 
            top-left-x 
            top-left-y                    
            bot-right-x 
            bot-right-y)))))
 
-(define merge-floating-layer
+;;; Procedure:
+;;;   merge-floating-layer!
+;;; Parameters:
+;;;   image, an image
+;;;   layer-merge, a positive integer
+;;;   merge-type, an integer
+;;; Purpose:
+;;;   To merge a layer with the layer below it.
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   merge-type must be 0, 1, or 2
+;;;   layer-merge is a layer in image
+;;; Postconditions:
+;;;   (get-top-layer image) is the previous top two layers merged together.
+(define merge-floating-layer!
   (lambda (image layer-merge merge-type)
     (gimp-image-merge-down image layer-merge merge-type)))
 
-(define blur-image
+;;; Procedure:
+;;;   blur-image
+;;; Parameters:
+;;;   image, an image
+;;;   layer, a positive integer
+;;;   repititions, a positive integer
+;;; Purpose:
+;;;   To blur the image.
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   layer is a layer in image
+;;; Postconditions:
+;;;   The image's state is equal to that of an image that has had
+;;;   plug-in-blur called on it repititions times.
+(define blur-image!
   (lambda (image layer repititions)
     (repeat repititions plug-in-blur 1 image layer)))
 
+;;; Procedure:
+;;;   distort!
+;;; Parameters:
+;;;   image, an image
+;;;   distort-lst, a list
+;;;   aoe, a number
+;;;   blur-degree, a number
+;;; Purpose:
+;;;   To distort the image by creating scaled ellipses on it and then
+;;;    blurring the image
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   The image will look distorted.
 (define distort!
   (lambda (image distort-lst aoe blur-degree)
     (render-blobs! image distort-lst
                    3
                    (round (find-biggest-distortion image))
                    aoe 0 #t)
-    (blur-image image (get-top-layer image) blur-degree)))
+    (blur-image! image (get-top-layer image) blur-degree)))
 
+;;; Procedure:
+;;;   magnifying-glass!
+;;; Parameters:
+;;;   image, an image
+;;;   magnify-lst, a list
+;;;   factor, a number
+;;; Purpose:
+;;;   To render "magnifying glasses" on the image (ellipses that magnify a 
+;;;   certain area)
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   factor > 0
+;;; Postconditions:
+;;;   [No additional]
 (define magnifying-glass!
   (lambda (image magnify-lst factor)
     (let kernel ([remaining magnify-lst])
       (if (null? remaining)
           (context-update-displays!)
           (let ([glass (car remaining)]
-                [max-diameter (round (find-biggest-magnifier image))])
+                [max-diameter (find-biggest-magnifier image)])
             (add-scaled-ellipse! 
              image (get-top-layer image)
              (modulo (round (car (list-ref glass 0))) (image-width image))
@@ -262,13 +371,35 @@
              (* -1 factor) #t)
             (kernel (cdr remaining)))))))
 
+;;; Procedure:
+;;;   render-blobs!
+;;; Parameters:
+;;;   image, an image
+;;;   lst, a list of column-vectors
+;;;   min-dimension, a number
+;;;   max-dimension, a number
+;;;   aoe, a number
+;;;   blur-degree, a number
+;;;   stroke?, a boolean
+;;; Purpose:
+;;;   To blur the background of image, and add (length lst) scaled ellipses
+;;;   to image (these ellipses are possibly outlined)
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   max-dimension > min-dimension
+;;; Postconditions:
+;;;   There are (length lst) scaled ellipses on image. For each ellipse e:
+;;;   (> max-dimension width-of-e min-dimension)
+;;;   (> max-dimension height-of-e min-dimension)
+;;;   (if stroke? (each ellipse has an outline) (no ellipse has an outline))
 (define render-blobs!
   (lambda (image lst min-dimension max-dimension aoe blur-degree stroke?)
     (copy-and-add-layer! image (get-top-layer image))
     (let* ([layers (cadr (gimp-image-get-layers image))]
            [base (cadr layers)]
            [blurred (car layers)])
-      (blur-image image blurred blur-degree)
+      (blur-image! image blurred blur-degree)
       (let kernel ([remaining lst])
         (if (null? remaining)
             (context-update-displays!)
@@ -283,6 +414,22 @@
                width height aoe stroke?)
               (kernel (cdr remaining))))))))
 
+;;; Procedure:
+;;;   rain-me!
+;;; Parameters:
+;;;   image, an image
+;;;   raindrop-lst, a list
+;;;   aoe, a number
+;;;   blur-degree, a number
+;;; Purpose:
+;;;   To render realistic looking raindrops on an image
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   Realistic rain will appear on the image, positions and lengths
+;;;   based off of raindrop-lst
 (define rain-me!
   (lambda (image raindrop-lst aoe blur-degree)
     (render-blobs! image raindrop-lst
@@ -290,6 +437,19 @@
                    (round (find-biggest-raindrop image))
                    aoe blur-degree #f)))
 
+;;; Procedure:
+;;;   copy-and-add-layer!
+;;; Parameters:
+;;;   image, an image
+;;;   layer, a positive integer
+;;; Purpose:
+;;;   To copy layer and add that copied layer to image
+;;; Produces:
+;;;   [Nothing; Called for the side effect.]
+;;; Preconditions:
+;;;   layer must be a layer in image
+;;; Postconditions:
+;;;   (get-top-layer image) will be a copy of layer.
 (define copy-and-add-layer!
   (lambda (image layer)
     (gimp-image-add-layer 
@@ -297,21 +457,71 @@
      (car (gimp-layer-copy layer 1))
      0)))
 
+;;; Procedure:
+;;;   get-top-layer
+;;; Parameters:
+;;;   image, an image
+;;; Purpose:
+;;;   To retrieve the number value of the top layer in image
+;;; Produces:
+;;;   layer, an integer
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   layer is the first element in the second element of 
+;;;   (gimp-image-get-layers image)
 (define get-top-layer
   (lambda (image)
     (caadr (gimp-image-get-layers image))))
 
+;;; Procedure:
+;;;   find-biggest-raindrop
+;;; Parameters:
+;;;   image, an image
+;;; Purpose:
+;;;   To retrieve the largest width/height that any raindrop is allowed to be
+;;; Produces:
+;;;   n, a number
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   [No additional]
 (define find-biggest-raindrop
   (lambda (image)
-    (* 2.4 (log (image-area image)))))
+    (round (* 2.4 (log (image-area image))))))
 
+;;; Procedure:
+;;;   find-biggest-distortion
+;;; Parameters:
+;;;   image, an image
+;;; Purpose:
+;;;   To retrieve the largest width/height that any distortion is allowed to be
+;;; Produces:
+;;;   n, a number
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   [No additional]
 (define find-biggest-distortion
   (lambda (image)
-    (* 4 (log (image-area image)))))
+    (round (* 4 (log (image-area image))))))
 
+;;; Procedure:
+;;;   find-biggest-magnifier
+;;; Parameters:
+;;;   image, an image
+;;; Purpose:
+;;;   To retrieve the largest width/height that any magnifying glass is allowed
+;;;   to be
+;;; Produces:
+;;;   n, a number
+;;; Preconditions:
+;;;   [No additional]
+;;; Postconditions:
+;;;   [No additional]
 (define find-biggest-magnifier
   (lambda (image)
-    (* 6 (log (image-area image)))))
+    (round (* 6 (log (image-area image))))))
 
 
 ; +---------------+--------------------------------------------------
